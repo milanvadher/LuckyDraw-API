b8688cdaa7a7e5904a1909283481cad22f739a08
AT User01 <atuser01@amp.local>
2018-09-15 11:10:10 +0530

    notifications

--- index.js
+++ index.js
@@ -38,6 +38,7 @@ MongoClient.connect(
         users = db.collection("users");
         drawSlots = db.collection("drawSlots");
         nextCouponNumber = db.collection("nextCouponNumber");
+        notifications = db.collection("notifications");
     }
 );
 
@@ -204,6 +205,42 @@ app.post("/forgotPassword", (req, res) => {
                     }
                 });
             } 
+        }
+    });
+});
+
+app.post("notify", (req, res) => {
+    notifications.insert({
+        created_at: (new Date).getTime(),
+        msg: req.body.msg
+    }, (err, res) => {
+        if(err) {
+            res.status(500).json({ err: "internal server error please try again later." });
+        } else {
+            res.send({ msg: 'Notification sent.' })
+        }
+    });
+});
+
+
+app.get("/getNotifications", (req, res) => {
+    users.findOne({contactNumber: req.body.contactNumber}, (err, res) => {
+        if(err) {
+            res.status(500).json({ err: "internal server error please try again later." });
+        } else {
+            let lastSeenDate = new Date();
+            lastSeenDate.setDate(lastSeenDate.getDate() - 1)
+            let lastSeenTime = lastSeenDate.getTime()
+            if(res.lastSeen) {
+                lastSeenTime = res.lastSeen;
+            } 
+            notifications.findMany({created_at: { $gte: lastSeenTime}}, (err, res) => {
+                if(err) {
+                    res.status(500).json({ err: "internal server error please try again later." });
+                } else {
+                    res.send({ msgs: res.toArray() })
+                }
+            });
         }
     });
 });
