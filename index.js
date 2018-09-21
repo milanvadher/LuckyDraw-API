19e3ebff3aa64c2a731f87f37f4e294bed84edf0
Gaurav Suri <gaurav08suri@gmail.com>
2018-09-21 22:11:30 +0530

    remove password

--- index.js
+++ index.js
@@ -490,6 +490,7 @@ app.post("/userList", (req, res) => {
             if (_err) {
                 res.send({ err: "Internal server error", _err })
             } else {
+                users.forEach(user => delete.user.password)
                 res.send(users);
             }
         });
